244407ef1db869d4158b572ebdba3e5a8c048674
TT <iloewen79@gmx.de>
2024-12-06 08:16:13 +0100

    Добавил инъекцию зависимостией в класс Lion

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -4,22 +4,24 @@ import java.util.List;
 
 public class Lion {
 
-    boolean hasMane;
+ private boolean hasMane;
+ private IndependentLion Lion;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, IndependentLion Lion) throws Exception {
+        this.Lion = Lion;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
 
-    Feline feline = new Feline();
+
 
     public int getKittens() {
-        return feline.getKittens();
+        return Lion.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,6 +29,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return Lion.getFood("Хищник");
     }
 }
