09949500e142b79e6752355f91ab887295aadec3
TT <iloewen79@gmx.de>
2024-12-07 19:34:52 +0100

    Ready to go

---
+++ src/test/java/com/example/AlexLionTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.invoke.WrongMethodTypeException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class AlexLionTest extends TestsSetUp{
+    protected AlexLion alexLion = new AlexLion(independentLion);
+    public AlexLionTest() throws Exception {
+    }
+
+    @Test
+    public void testGetFriends()throws Exception{
+        assertEquals(List.of("Зебра Марти, Бегемотиха Глория, Жираф Мелман"),alexLion.getFriends());
+    }
+
+    @Test
+    public void testGetPlaceOfLiving()throws Exception{
+        assertEquals("Нью-Йоркский зоопарк",alexLion.getPlaceOfLiving());
+    }
+
+    @Test
+    public void testGetKittens()throws Exception{
+        assertEquals(0,alexLion.getKittens());
+    }
+}
